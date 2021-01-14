c46680a6cbebeab75782b3f640218d97396385d6
Evan Moreau <ekmoreau@gmail.com>
2021-01-14 11:52:57 -0500

    Commit

--- foundation/src/assets/js/app.js
+++ foundation/src/assets/js/app.js
@@ -37,33 +37,29 @@ $(".slider--single").slick({
 });
 
 $('.slider--3up').slick({dots: true,
-  infinite: false,
+  infinite: true,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
   dots: true,
+  arrows: false,
   responsive: [
     {
       breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
+      // settings: {
+      //   dots: true
+      // }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
       }
     }
     // You can unslick at a given breakpoint now by adding:
